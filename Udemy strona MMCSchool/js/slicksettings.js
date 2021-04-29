f51d578af1be7d67936df92ee0443c22ef5f53e0
qbxof93 <jakrob93@outlook.com>
2021-04-29 17:40:31 +0200

    udemy kurs

---
+++ Udemy strona MMCSchool/js/slicksettings.js
@@ -0,0 +1,22 @@
+$('.team-carousel').slick({
+	autoplay: true,
+	mobileFirst: true,
+	arrows: false,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
+});
